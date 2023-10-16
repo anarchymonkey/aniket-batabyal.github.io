78ded8ea857f1aaf83d61ebc208cd2354422cb01
anarchymonkey <aniketbatabyal1996@gmail.com>
2023-10-16 19:37:35 +0530

    added portfolio

---
+++ index.js
@@ -0,0 +1,22 @@
+const hoverElement = document.getElementById('gradWrapper');
+
+
+document.addEventListener('mousemove', (event) => {
+    const clientX = event.clientX;
+    const clientY = event.clientY;
+
+    hoverElement.style.setProperty('background', `radial-gradient(600px at ${clientX + 'px'} ${clientY + 'px'}, rgba(234, 78, 216, 0.15), transparent 80%)`);
+})
+
+function goToLink(link) {
+    window.open(link, "_blank");
+}
+
+function scrollToBottom() {
+    const rightPane = document.querySelector('.rightPane');
+    rightPane.scrollTop = rightPane.scrollHeight;
+    rightPane.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+      });
+}
